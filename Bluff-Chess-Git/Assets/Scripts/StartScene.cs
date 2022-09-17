ef8de45cb759bfea286526100e928c73573db161
selimtanrikulu <tanrikuluselim@hotmail.com>
2022-09-17 20:09:10 +0300

    17septv3

---
+++ Bluff-Chess-Git/Assets/Scripts/StartScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    [SerializeField] float startAfter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(startAfter < 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }   
+        else
+        {
+            startAfter -= Time.deltaTime;
+        }
+    }
+}
