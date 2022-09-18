f86cf39c1f6aab253d5f869ad8afdd15b33bf675
selimtanrikulu <tanrikuluselim@hotmail.com>
2022-09-18 22:32:48 +0300

    sept18

---
+++ Bluff-Chess-Git/Assets/Scripts/Trailer/ObjectMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    [SerializeField] float startMovementSpeed;
+    [SerializeField] float acceleration;
+
+    [SerializeField] Vector3 direction;
+    [SerializeField] float startTime;
+
+    float timePast = 0;
+    float currentMovementSpeed = 0;
+
+    void Update()
+    {
+        timePast += Time.deltaTime;
+
+        if(startTime <= timePast)
+        {
+            currentMovementSpeed += acceleration * Time.deltaTime;
+
+
+            Vector3 pos = transform.position;
+            pos += direction * currentMovementSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+
+
+    }
+}
