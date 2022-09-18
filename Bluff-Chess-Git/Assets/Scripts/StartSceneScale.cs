4ade1a96141e73321e507da0f5a9534e4a7f40a7
selimtanrikulu <tanrikuluselim@hotmail.com>
2022-09-18 13:24:59 +0300

    18septv5

--- Bluff-Chess-Git/Assets/Scripts/StartSceneScale.cs
+++ Bluff-Chess-Git/Assets/Scripts/StartSceneScale.cs
@@ -6,6 +6,11 @@ public class StartSceneScale : MonoBehaviour
 {
     Camera cam;
 
+    float magicalHeight = 1334;
+    float magicalWidth = 750;
+    
+    float magicalScale = 2.1f;
+
 
     void Start()
     {
@@ -13,10 +18,33 @@ public class StartSceneScale : MonoBehaviour
             return;
         #endif
 
-        float newScale = 0;
+
 
         float width = Screen.currentResolution.width;
         float height = Screen.currentResolution.height;
+
+        if(width > magicalWidth)
+        {
+            if(height > magicalHeight && height - magicalHeight > width - magicalWidth)
+            {
+                float newScale = (height/magicalHeight) * magicalScale;
+                GetComponent<RectTransform>().localScale = new Vector3(newScale,newScale,newScale);
+            }
+            else
+            {
+                float newScale = (width/magicalWidth) * magicalScale;
+                GetComponent<RectTransform>().localScale = new Vector3(newScale,newScale,newScale);
+            }
+
+            
+
+        }
+
+
+        if(height > magicalHeight)
+        {
+
+        }
 
     }
 }
